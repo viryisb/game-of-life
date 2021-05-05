82b5d6e618ea8552335eed1eb44516f016c33d04
viryisb <bernasconivir@gmail.com>
2021-05-05 02:26:43 -0300

    start simulation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React, { useState } from "react";
+import React, { useCallback, useRef, useState } from "react";
 import produce from 'immer'
 import "./App.css";
 
 
 const numRows = 50;
 const numCols = 50;
+
+const operations = [
+  [0, 1],
+  [0, -1],
+  [1, -1],
+  [-1, 1],
+  [1, 1],
+  [-1, -1],
+  [1, 0],
+  [-1, 0]
+
+]
 
 const App = () => {
   const [grid, setGrid] = useState(() => {
@@ -15,12 +27,52 @@ const App = () => {
     return rows;
   });
   const [running, setRunning] = useState(false)
+  const runningRef = useRef(running);
+  runningRef.current = running
+
+  const runSimulation = useCallback(() => {
+
+    if (!runningRef.current) {
+      return;
+    }
+    //current value of grid and return new value
+    setGrid((g) => {
+      return produce(g, gridCopy => {
+        for (let i = 0; i < numRows; i++) {
+          for (let k = 0; k < numCols; k++) {
+            let neighbors = 0;
+            operations.forEach(([x, y]) => {
+              const newI = i + x;
+              const newK = k + y;
+              if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
+                neighbors += g[newI][newK];
+              }
+            });
+
+            if (neighbors < 2 || neighbors > 3) {
+              gridCopy[i][k] = 0;
+            } else if (g[i][k] === 0 && neighbors === 3) {
+              gridCopy[i][k] = 1;
+            }
+          }
+        }
+      })
+    })
+   
+
+    setTimeout(runSimulation, 1000)
+  }, [])
 
   return (
     <>
       <button
         onClick={() => {
-          setRunning(!running)
+          setRunning(!running);
+          if (!running) {
+            runningRef.current = true;
+            runSimulation();
+          }
+
         }}
       >
         {running ? "stop" : "start"}</button>
