13d9f8a2798d3d105be0d8ecd5a94c6f22e4589e
viryisb <bernasconivir@gmail.com>
2021-05-04 23:33:00 -0300

    set array of 50

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { useState } from "react";
 import "./App.css";
 
+//Start with 50 then will see
 const numRows = 50;
 const numCols = 50;
 
 const App = () => {
-  const [grid, setGrid] = useState(() => {
-    const rows = [];
-    for (let i = 0; i < numRows; i++) {
-      rows.push(Array(numCols), () => 0);
-    }
-    return rows;
+  const [grid, setGrid] = useState(()=>{
+  const rows=[];
+  for(let i=0; i<numRows; i++){
+    rows.push(Array.from(Array(numCols), ()=>0))
+  }
+  return rows;
   });
-  return <div>{}</div>;
-};
 console.log(grid)
+return <div>hello</div>
+};
 export default App;
